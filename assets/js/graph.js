24c94d6ea1111fa8c09adc6c729b697b8e931ade
lostsh <0pdlna3@gmail.com>
2021-01-22 15:51:07 +0100

    adding base js

---
+++ assets/js/graph.js
@@ -0,0 +1,23 @@
+
+function main(dataFilePath){
+    var p = document.createElement("p");
+    p.textContent = "coucou je suis le futur graphique !<br> ["+dataFilePath+"]<br>"+readFile(dataFilePath);
+
+    document.querySelector("#articleContainer").appendChild(p);
+}
+
+
+/**
+ * Read file return content
+ * @apram file path
+ * @return row file content
+ */
+function readFile(fille){
+    var file = new XMLHttpRequest();
+    var content;
+    file.open("GET", fille, false);
+    if(file.readyState === 4){
+      content = file.responseText;
+    }
+    return content;
+  }
