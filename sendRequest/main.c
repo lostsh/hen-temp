7c9963ed5bcd02c85a8aed9cd9c94eb1e78374c3
lostsh <0pdlna3@gmail.com>
2021-01-23 17:33:45 +0100

    start request c program

--- sendRequest/main.c
+++ sendRequest/main.c
@@ -1,1 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "secret.h"
+
+int main(int argc, char **argv){
+    if(argc > 2){
+    }else{
+        printf("[-] Please send a value. For exemple :\n\t%s 17.5\n\tTo send 17.5\n", argv[0]);
+    }
+    return EXIT_SUCCESS;
+}
